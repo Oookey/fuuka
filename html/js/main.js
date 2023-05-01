846205788d047f4a49ea43cec0d9b15d7fa090c4
平賀 舞子 <hiraga@oookey.co.jp>
2023-05-01 17:14:32 +0900

    【html】トップページ

--- html/js/main.js
+++ html/js/main.js
@@ -1,1 +1,32 @@
 // main.js
+// ハンバーガーメニュー
+$(".hamburger").click(function () {//ボタンがクリックされたら
+	$(this).toggleClass('active');//ボタン自身に activeクラスを付与し
+    $("#g-nav").toggleClass('panelactive');//ナビゲーションにpanelactiveクラスを付与
+    $("#g-nav").next().slideToggle(100);
+});
+
+$(".nav_sub a").click(function () {//ナビゲーションのリンクがクリックされたら
+    $(".hamburger").removeClass('active');//ボタンの activeクラスを除去し
+    $("#g-nav").removeClass('panelactive');//ナビゲーションのpanelactiveクラスも除去
+});
+
+// topics
+const mySwiper = new Swiper('.card02 .swiper', {
+  slidesPerView: 'auto',
+  spaceBetween: 16,
+  grabCursor: true,
+  pagination: {
+    el: '.card02 .swiper-pagination',
+    clickable: true,
+  },
+  navigation: {
+    nextEl: '.card02 .swiper-button-next',
+    prevEl: '.card02 .swiper-button-prev',
+  },
+  breakpoints: {
+    1025: {
+      spaceBetween: 32,
+    }
+  },
+});
