9b8155127880080a3be8101b1ca2d95a18fb790b
平賀 舞子 <hiraga@oookey.co.jp>
2023-05-09 19:39:10 +0900

    【html】トップページ

--- html/js/main.js
+++ html/js/main.js
@@ -1,3 +1,13 @@
+// kirigirisu
+(function(d) {
+  var config = {
+    kitId: 'jfc4tfq',
+    scriptTimeout: 3000,
+    async: true
+  },
+  h=d.documentElement,t=setTimeout(function(){h.className=h.className.replace(/\bwf-loading\b/g,"")+" wf-inactive";},config.scriptTimeout),tk=d.createElement("script"),f=false,s=d.getElementsByTagName("script")[0],a;h.className+=" wf-loading";tk.src='https://use.typekit.net/'+config.kitId+'.js';tk.async=true;tk.onload=tk.onreadystatechange=function(){a=this.readyState;if(f||a&&a!="complete"&&a!="loaded")return;f=true;clearTimeout(t);try{Typekit.load(config)}catch(e){}};s.parentNode.insertBefore(tk,s)
+})(document);
+
 // main.js
 // ハンバーガーメニュー
 $(".hamburger").click(function () {//ボタンがクリックされたら
@@ -47,5 +57,24 @@ $(function(){
       variableWidth: true,
       autoplaySpeed:1500,
       arrows:false,
+  });
+});
+
+// 体験宿泊プラン
+$(function(){
+  $('.slick_exp').slick({
+      autoplay: true,//自動的に動き出すか。初期値はfalse。
+      infinite: true,//スライドをループさせるかどうか。初期値はtrue。
+      speed: 1300,//スライドのスピード。初期値は300。
+      autoplaySpeed:5000,
+      slidesToShow: 1,//スライドを画面に3枚見せる
+      slidesToScroll: 1,//1回のスクロールで1枚の写真を移動して見せる
+      arrows:true,
+      prevArrow: '<div class="slick-prev"></div>',//矢印部分PreviewのHTMLを変更
+      nextArrow: '<div class="slick-next"></div>',//矢印部分NextのHTMLを変更
+      centerMode: true,//要素を中央ぞろえにする
+      variableWidth: true,//幅の違う画像の高さを揃えて表示
+      dots: true,//下部ドットナビゲーションの表示
+      dotsClass: "slick_exp-dots",
   });
 });
