efeced9cfd562b6b0e77adc56e607f2ca24c592c
LO KA WENG <weng@oookey.co.jp>
2023-05-25 12:55:29 +0900

    【WP】周辺観光

---
+++ wp-content/themes/fuuka/archive-tourism.php
@@ -0,0 +1,260 @@
+<?php
+/**
+* Template Name: 周辺観光
+*/
+get_header(); ?>
+<!------------- HEADER ------------->
+
+<body>
+    <!-- ↓↓↓　本番時コメントアウトを外す　↓↓↓ -->
+    <!-- Google Tag Manager (noscript) -->
+    <!-- <noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-NCVNXH8"
+    height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript> -->
+    <!-- End Google Tag Manager (noscript) -->
+
+    <!-- ↓↓↓ HEADER ↓↓↓ -->
+    <header>
+        <!-- main menu -->
+        <?php get_sidebar('main_menu'); ?>
+
+        <!-- fix menu -->
+        <?php get_sidebar('flex_menu'); ?>    
+    </header>
+    <!-- ↑↑↑ HEADER ↑↑↑ -->
+
+    <!-- ↓↓↓ MAIN ↓↓↓ -->
+    <main id="tourism" class="subpage">
+        <!-- mv -->
+        <div class="top_fv">
+            <div class="top_fv_inner">
+                <div class="top_fv_movie">
+                    <div class="in"> 
+                        <img src="<?php bloginfo('template_directory'); ?>/image/tourism_mv.png" alt="" class="movie">
+                    </div>
+                </div>
+                <div class="fv_menu pc">
+                    <a href="https://www.instagram.com/hotel_fuka/?hl=ja" class="fv_menu_inner fv_fadein" target="_blank" rel="noopener">
+                        <img class="fv_menu_icon" src="<?php bloginfo('template_directory'); ?>/image/icon_instagram_fv.svg" alt="">
+                    </a>
+                    <a href="<?php bloginfo('url'); ?>/index.html#access" class="fv_menu_inner fv_fadein">
+                        <img class="fv_menu_icon" src="<?php bloginfo('template_directory'); ?>/image/icon_map_fv.svg" alt="">
+                    </a>
+                </div>
+            </div>
+        </div>
+
+        <!-- 導入文 -->
+        <div class="pageinfo bg_b_c">
+            <div class="c_info_bg inner fv_fadein">
+                <div class="pageinfo_inner">
+                    <img src="<?php bloginfo('template_directory'); ?>/image/pagetitle_tourism.png" alt="">
+                </div>
+            </div>
+        </div>
+
+        <!--  -->
+        <div class="content bg_wave">
+            <div class="inner">
+                <div class="illust fv_fadein"><img src="<?php bloginfo('template_directory'); ?>/image/tourism_fv_illust.svg" alt=""></div>
+                <div class="con-sp-fv fadein">
+                    <div class="float-l con_mv">
+                        <div class="zoom" style="border-radius: 3.8rem;">
+                            <img src="<?php bloginfo('template_directory'); ?>/image/tourism_photo_main1.png" alt="">
+                        </div>
+                        <ul class="slick">
+                            <li><img src="<?php bloginfo('template_directory'); ?>/image/tourism_photo_1-1.png"></li>
+                            <li><img src="<?php bloginfo('template_directory'); ?>/image/tourism_photo_1-2.png"></li>
+                        </ul>
+                    </div>
+                    <div class="float-r con_text-r">
+                        <div class="con_title">
+                            <h2>カナディアンキャンプ</h2>
+                        </div>
+                    </div>
+                </div>
+                <div class="float-r con_text-r fadein">
+                    <p class="text">
+                        初心者向けの体験乗馬プラン、八ヶ岳の四季を五感で楽しめる豊富な外乗コース、初級者から上級者までレベルに合わせたレッスンが楽しめます。
+                    </p>
+                    <dl class="about_point_wrap">
+                        <dt>所在地：</dt>
+                        <dd>山梨県北杜市小淵沢町10176-12</dd>
+                        <dt>主要地からのアクセス：</dt>
+                        <dd>小淵沢ICから車で5分</dd>
+                        <dt>当館からのアクセス：</dt>
+                        <dd>車で約11分</dd>
+                        <dt>公式サイト：</dt>
+                        <dd><a href="http://www.canacan.jp" target="_blank" rel="noopener">http://www.canacan.jp</a></dd>
+                    </dl>
+                    <a href="" class="flex">
+                        <p class="btn"><span class="arrow"></span>「カナディアンキャンプ」乗馬体験付きプランはこちら</p>
+                    </a>
+                </div>                
+            </div>
+        </div>
+
+        <!--  -->
+        <div class="content bg_b_c">
+            <div class="inner">
+                <div class="con-sp-fv fadein">
+                    <div class="float-l con_mv">
+                        <div class="zoom" style="border-radius: 3.8rem;">
+                            <img src="<?php bloginfo('template_directory'); ?>/image/tourism_photo_main2.png" alt="">
+                        </div>
+                        <ul class="slick">
+                            <li><img src="<?php bloginfo('template_directory'); ?>/image/tourism_photo_2-1.png"></li>
+                            <li><img src="<?php bloginfo('template_directory'); ?>/image/tourism_photo_2-2.png"></li>
+                        </ul>
+                    </div>
+                    <div class="float-r con_text-r">
+                        <div class="con_title">
+                            <h2>カナディアンキャンプ</h2>
+                        </div>
+                    </div>
+                </div>
+                <div class="float-r con_text-r fadein">
+                    <p class="text">
+                        初心者向けの体験乗馬プラン、八ヶ岳の四季を五感で楽しめる豊富な外乗コース、初級者から上級者までレベルに合わせたレッスンが楽しめます。
+                    </p>
+                    <dl class="about_point_wrap">
+                        <dt>所在地：</dt>
+                        <dd>山梨県北杜市小淵沢町10176-12</dd>
+                        <dt>主要地からのアクセス：</dt>
+                        <dd>小淵沢ICから車で5分</dd>
+                        <dt>当館からのアクセス：</dt>
+                        <dd>車で約11分</dd>
+                        <dt>公式サイト：</dt>
+                        <dd><a href="http://www.canacan.jp" target="_blank" rel="noopener">http://www.canacan.jp</a></dd>
+                    </dl>
+                    <a href="" class="flex">
+                        <p class="btn"><span class="arrow"></span>「カナディアンキャンプ」乗馬体験付きプランはこちら</p>
+                    </a>
+                </div>         
+            </div>
+        </div>
+
+        <!--  -->
+        <div class="content bg_wave">
+            <div class="inner">
+                <div class="con-sp-fv fadein">
+                    <div class="float-l con_mv">
+                        <div class="zoom" style="border-radius: 3.8rem;">
+                            <img src="<?php bloginfo('template_directory'); ?>/image/tourism_photo_main1.png" alt="">
+                        </div>
+                        <ul class="slick">
+                            <li><img src="<?php bloginfo('template_directory'); ?>/image/tourism_photo_1-1.png"></li>
+                            <li><img src="<?php bloginfo('template_directory'); ?>/image/tourism_photo_1-2.png"></li>
+                        </ul>
+                    </div>
+                    <div class="float-r con_text-r">
+                        <div class="con_title">
+                            <h2>カナディアンキャンプ</h2>
+                        </div>
+                    </div>
+                </div>
+                <div class="float-r con_text-r fadein">
+                    <p class="text">
+                        初心者向けの体験乗馬プラン、八ヶ岳の四季を五感で楽しめる豊富な外乗コース、初級者から上級者までレベルに合わせたレッスンが楽しめます。
+                    </p>
+                    <dl class="about_point_wrap">
+                        <dt>所在地：</dt>
+                        <dd>山梨県北杜市小淵沢町10176-12</dd>
+                        <dt>主要地からのアクセス：</dt>
+                        <dd>小淵沢ICから車で5分</dd>
+                        <dt>当館からのアクセス：</dt>
+                        <dd>車で約11分</dd>
+                        <dt>公式サイト：</dt>
+                        <dd><a href="http://www.canacan.jp" target="_blank" rel="noopener">http://www.canacan.jp</a></dd>
+                    </dl>
+                    <a href="" class="flex">
+                        <p class="btn"><span class="arrow"></span>「カナディアンキャンプ」乗馬体験付きプランはこちら</p>
+                    </a>
+                </div>            
+            </div>
+        </div>
+
+        <!--  -->
+        <div class="content bg_b_c">
+            <div class="inner">
+                <div class="con-sp-fv fadein">
+                    <div class="float-l con_mv">
+                        <div class="zoom" style="border-radius: 3.8rem;">
+                            <img src="<?php bloginfo('template_directory'); ?>/image/tourism_photo_main2.png" alt="">
+                        </div>
+                        <ul class="slick">
+                            <li><img src="<?php bloginfo('template_directory'); ?>/image/tourism_photo_2-1.png"></li>
+                            <li><img src="<?php bloginfo('template_directory'); ?>/image/tourism_photo_2-2.png"></li>
+                        </ul>
+                    </div>
+                    <div class="float-r con_text-r">
+                        <div class="con_title">
+                            <h2>カナディアンキャンプ</h2>
+                        </div>
+                    </div>
+                </div>
+                <div class="float-r con_text-r fadein">
+                    <p class="text">
+                        初心者向けの体験乗馬プラン、八ヶ岳の四季を五感で楽しめる豊富な外乗コース、初級者から上級者までレベルに合わせたレッスンが楽しめます。
+                    </p>
+                    <dl class="about_point_wrap">
+                        <dt>所在地：</dt>
+                        <dd>山梨県北杜市小淵沢町10176-12</dd>
+                        <dt>主要地からのアクセス：</dt>
+                        <dd>小淵沢ICから車で5分</dd>
+                        <dt>当館からのアクセス：</dt>
+                        <dd>車で約11分</dd>
+                        <dt>公式サイト：</dt>
+                        <dd><a href="http://www.canacan.jp" target="_blank" rel="noopener">http://www.canacan.jp</a></dd>
+                    </dl>
+                    <a href="" class="flex">
+                        <p class="btn"><span class="arrow"></span>「カナディアンキャンプ」乗馬体験付きプランはこちら</p>
+                    </a>
+                </div>           
+            </div>
+        </div>
+
+        <!--  -->
+        <div class="content bg_wave">
+            <div class="inner">
+                <div class="con-sp-fv fadein">
+                    <div class="float-l con_mv">
+                        <div class="zoom" style="border-radius: 3.8rem;">
+                            <img src="<?php bloginfo('template_directory'); ?>/image/tourism_photo_main1.png" alt="">
+                        </div>
+                        <ul class="slick">
+                            <li><img src="<?php bloginfo('template_directory'); ?>/image/tourism_photo_1-1.png"></li>
+                            <li><img src="<?php bloginfo('template_directory'); ?>/image/tourism_photo_1-2.png"></li>
+                        </ul>
+                    </div>
+                    <div class="float-r con_text-r">
+                        <div class="con_title">
+                            <h2>カナディアンキャンプ</h2>
+                        </div>
+                    </div>
+                </div>
+                <div class="float-r con_text-r fadein">
+                    <p class="text">
+                        初心者向けの体験乗馬プラン、八ヶ岳の四季を五感で楽しめる豊富な外乗コース、初級者から上級者までレベルに合わせたレッスンが楽しめます。
+                    </p>
+                    <dl class="about_point_wrap">
+                        <dt>所在地：</dt>
+                        <dd>山梨県北杜市小淵沢町10176-12</dd>
+                        <dt>主要地からのアクセス：</dt>
+                        <dd>小淵沢ICから車で5分</dd>
+                        <dt>当館からのアクセス：</dt>
+                        <dd>車で約11分</dd>
+                        <dt>公式サイト：</dt>
+                        <dd><a href="http://www.canacan.jp" target="_blank" rel="noopener">http://www.canacan.jp</a></dd>
+                    </dl>
+                    <a href="" class="flex">
+                        <p class="btn"><span class="arrow"></span>「カナディアンキャンプ」乗馬体験付きプランはこちら</p>
+                    </a>
+                </div>           
+            </div>
+        </div>
+
+    </main>
+    <!-- ↑↑↑ MAIN ↑↑↑ -->
+
+    <!-- ↓↓↓ FOOTER ↓↓↓ -->
+    <?php get_footer(); ?>
