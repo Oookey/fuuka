4ec653ec4083c130b60c9d8d416149f3d126e194
LO KA WENG <weng@oookey.co.jp>
2023-05-25 19:55:40 +0900

    【WP】宿泊プラン

--- wp-content/themes/fuuka/archive-activity.php
+++ wp-content/themes/fuuka/archive-activity.php
@@ -74,7 +74,7 @@ get_header(); ?>
         while (have_posts()) : the_post();
         ?>
 
-        <div class="content bg_wave about1">
+        <div class="content roop_bg about1">
             <div class="inner">
                 <div class="con-sp-fv fadein">
                     <div class="float-l con_mv">
