e3380d358047e19dc40ebc209a987b7a00a0976b
LO KA WENG <weng@oookey.co.jp>
2023-05-30 12:25:10 +0900

    【WP】トップページ

--- wp-content/themes/fuuka/index.php
+++ wp-content/themes/fuuka/index.php
@@ -91,7 +91,7 @@
             </div>
             <!-- news -->
             <?php
-            query_posts('post_type=news&posts_per_page=1');
+            query_posts('post_type=news&posts_per_page=1&orderby=date&order=DESC');
             while (have_posts()) : the_post();
             ?>
                 <div class="top_news shadow fadein">
@@ -335,7 +335,7 @@
                     <h2 class="title">お知らせ</h2>
                     <dl class="">
                         <?php
-                        query_posts('post_type=news&posts_per_page=5');
+                        query_posts('post_type=news&posts_per_page=5&orderby=date&order=DESC');
                         while (have_posts()) : the_post();
                         ?>
 
