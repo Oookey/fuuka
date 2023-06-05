97ed060d4ce0981df7b0fb6ed0e48fc1e6dc2894
LO KA WENG <weng@oookey.co.jp>
2023-06-05 13:22:53 +0900

    【WP】Topic詳細スタイル、Topicのトップ表示

--- wp-content/themes/fuuka/single-topic.php
+++ wp-content/themes/fuuka/single-topic.php
@@ -50,11 +50,11 @@ get_header(); ?>
 								<?php while(have_rows('topic_detail_inner')): the_row(); ?>
 
                                 <?php if( get_sub_field('topic_detail_subtitle') ) { ?>
-                                    <h4 class="news_detail_subtitle"><?php the_sub_field('topic_detail_subtitle'); ?></h4>
+                                    <h4 class="news_subtitle"><?php the_sub_field('topic_detail_subtitle'); ?></h4>
                                 <?php } ?>
 
                                 <?php if( get_sub_field('topic_detail_img') ) { ?>
-                                    <div class="news_detail_img">
+                                    <div class="news_img">
                                         <img src="<?php the_sub_field('topic_detail_img'); ?>">
                                         <a href="<?php the_sub_field('topic_detail_img'); ?>" data-lightbox="img" data-title="<?php the_sub_field('topic_detail_img_info'); ?>" class="topic_detail_img_size">
                                             <p>画像を拡大</p>
