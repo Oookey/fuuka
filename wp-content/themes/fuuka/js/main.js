b8b2259d1ba175ae9d78766cf54f428d36fc576f
平賀 舞子 <hiraga@oookey.co.jp>
2023-06-06 17:26:14 +0900

    【WP】topic

--- wp-content/themes/fuuka/js/main.js
+++ wp-content/themes/fuuka/js/main.js
@@ -210,22 +210,9 @@ const slideLength = document.querySelectorAll('.insta_slide .swiper-a .sbi .swip
   }); 
 
   // 文字数制限
-  // const el = document.getElementById('topic_text');
-  // const str = el.textContent;
-  // const len = 46;
-  // if(str.length > len){
-  //     el.textContent = str.substring(0, len)+'…続きを読む';
-  // }
-
-
-let contents = document.getElementById('topic_text');
-contents.forEach(content => {
-  content.textContent = omittedContent(content.textContent);
-});
-function omittedContent(string) {
-  const MAX_LENGTH = 46;
-  if (string.length > MAX_LENGTH) {
-    return string.substr(0, MAX_LENGTH) + '…続きを読む';
+  const el = document.getElementById('topic_text,plan_text');
+  const str = el.textContent;
+  const len = 46;
+  if(str.length > len){
+      el.textContent = str.substring(0, len)+'…続きを読む';
   }
-  return string;
-}
