b3cac86defefbd35520b2a8b22f90987e838d670
LO KA WENG <weng@oookey.co.jp>
2023-06-08 09:51:44 +0900

    【WP】トップムービー

--- wp-content/themes/fuuka/index.php
+++ wp-content/themes/fuuka/index.php
@@ -29,7 +29,9 @@ get_header(); ?>
                     <div class="in"> 
                         <!-- <img src="<?php bloginfo('template_directory'); ?>/image/top_mv.png" alt="" class="movie"> -->
                         <video class="movie pc" src="<?php bloginfo('template_directory'); ?>/movie/index_movie.mp4" autoplay="" muted="" loop="" playsinline=""></video>
-                        <video class="movie sp" src="<?php bloginfo('template_directory'); ?>/movie/index_movie.mp4" autoplay="" muted="" loop="" playsinline=""></video>
+
+                        <video class="movie sp" src="<?php bloginfo('template_directory'); ?>/movie/index_movie_sp.mp4" autoplay="" muted="" loop="" playsinline=""></video>
+                        
                     </div>
                 </div>
                 <!-- ↓↓↓　動画に差し替え時コメントアウト　↓↓↓ -->
