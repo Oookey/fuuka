e39e6cb143340e47fa7c51f6c009911dcfe48813
平賀 舞子 <hiraga@oookey.co.jp>
2024-04-03 11:13:02 +0900

    【WP】客室修正

--- wp-content/themes/fuuka/page-room.php
+++ wp-content/themes/fuuka/page-room.php
@@ -133,8 +133,6 @@ get_header(); ?>
             </div>
         </div>
 
-<!-- ーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーー -->
-
         <!-- 温泉檜風呂付客室 -->
         <div class="content bg_b_c">
             <div class="inner img_bg_1">
@@ -205,8 +203,6 @@ get_header(); ?>
             </div>
         </div>
 
-<!-- ーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーー -->
-
         <!-- 星見ジャグジー風呂付客室 -->
         <div class="content bg_wave room3">
             <div class="inner img_bg_2">
@@ -274,10 +270,10 @@ get_header(); ?>
         </div>
 
         <!-- 美容FUUKAジュニアスイート --> 
-        <div class="content bg_wave room3">
+        <div class="content bg_b_c room4">
             <div class="inner img_bg_3">
                 <div class="con-sp-fv fadein">
-                    <div class="float-r con_mv">
+                    <div class="float-l con_mv">
                         <div class="zoom" style="border-radius: 3.8rem;">
                             <img src="<?php bloginfo('template_directory'); ?>/image/room_photo_main3.png" alt="">
                         </div>
@@ -287,7 +283,7 @@ get_header(); ?>
                             <p>・当ホテルはお部屋の布団敷きはセルフサービスとなります</p>
                         </div>
                     </div>
-                    <div class="float-l con_text-l">
+                    <div class="float-r con_text-r">
                         <div class="con_title">
                             <p>プライベートバス付客室</p>
                             <h2>
@@ -297,7 +293,7 @@ get_header(); ?>
                         </div>
                     </div>
                 </div>
-                <div class="float-l con_text-l fadein">
+                <div class="float-r con_text-r fadein">
                     <p class="text">
                     2020年新設の新しいおしゃれなバス付のお部屋。美容グッズもとりそろえています。<br>
                     女性グループやカップルでのご利用に特におすすめです。<br><br>
@@ -322,29 +318,14 @@ get_header(); ?>
                         <p>・当ホテルはお部屋の布団敷きはセルフサービスとなります</p>
                     </div>
                 </div>
-                <!-- アニメーション -->
-                <svg xmlns="http://www.w3.org/2000/svg" version="1.1" viewBox="0 0 500 500" width="100%" id="blobSvg" filter="blur(0px)" transform="rotate(0)" class="room_mask room_mask4">
-                    <image x="0" y="0" width="100%" height="100%" clip-path="url(#shape)" href="<?php bloginfo('template_directory'); ?>/image/mask_room4.png" preserveAspectRatio="none"/>
-                    <defs>
-                        <linearGradient id="gradient" x1="0%" y1="0%" x2="0%" y2="100%">
-                            <stop offset="0%" style="stop-color: rgb(238, 205, 163);"/>
-                            <stop offset="100%" style="stop-color: rgb(239, 98, 159);"/>
-                        </linearGradient>
-                    </defs>
-                    <clipPath id="shape">
-                        <path id="blob" fill="url(#gradient)">
-                            <animate attributeName="d" dur="9.5s" repeatCount="indefinite" values="M440.5,320.5Q418,391,355.5,442.5Q293,494,226,450.5Q159,407,99,367Q39,327,31.5,247.5Q24,168,89,125.5Q154,83,219.5,68Q285,53,335.5,94.5Q386,136,424.5,193Q463,250,440.5,320.5Z;M453.78747,319.98894Q416.97789,389.97789,353.96683,436.87838Q290.95577,483.77887,223.95577,447.43366Q156.95577,411.08845,105.64373,365.97789Q54.33169,320.86732,62.67444,252.61056Q71.01719,184.3538,113.01965,135.21007Q155.02211,86.06634,220.52211,66.46683Q286.02211,46.86732,335.5,91.94472Q384.97789,137.02211,437.78747,193.51106Q490.59704,250,453.78747,319.98894Z;M411.39826,313.90633Q402.59677,377.81265,342.92059,407.63957Q283.24442,437.46649,215.13648,432.5428Q147.02853,427.61911,82.23325,380.9572Q17.43796,334.29529,20.45223,250.83809Q23.46649,167.38089,82.5856,115.05707Q141.70471,62.73325,212.19045,63.73015Q282.67618,64.72705,352.67308,84.79839Q422.66998,104.86972,421.43486,177.43486Q420.19974,250,411.39826,313.90633Z;M440.5,320.5Q418,391,355.5,442.5Q293,494,226,450.5Q159,407,99,367Q39,327,31.5,247.5Q24,168,89,125.5Q154,83,219.5,68Q285,53,335.5,94.5Q386,136,424.5,193Q463,250,440.5,320.5Z;"/>
-                        </path>
-                    </clipPath>
-                </svg>
             </div>
         </div>
 
         <!-- スーペリア和洋ルーム -->
-        <div class="content bg_b_c room4">
+        <div class="content bg_wave room5">
             <div class="inner img_bg_4">
                 <div class="con-sp-fv fadein">
-                    <div class="float-l con_mv">
+                    <div class="float-r con_mv">
                         <div class="zoom" style="border-radius: 3.8rem;">
                             <img src="<?php bloginfo('template_directory'); ?>/image/room_photo_main4.png" alt="">
                         </div>
@@ -355,7 +336,7 @@ get_header(); ?>
                             <p>・当ホテルはお部屋の布団敷きはセルフサービスとなります</p>
                         </div>
                     </div>
-                    <div class="float-r con_text-r">
+                    <div class="float-l con_text-l">
                         <div class="con_title">
                             <p>ワンランク上の和洋室タイプのお部屋</p>
                             <h2>
@@ -365,7 +346,7 @@ get_header(); ?>
                         </div>
                     </div>
                 </div>
-                <div class="float-r con_text-r fadein">
+                <div class="float-l con_text-l fadein">
                     <p class="text">
                         ゆったりセミダブルサイズのベッド2台と、畳スペースには最大4枚まで<br class="pc">お布団を敷くことができ、最大6名様までお泊りいただけます。<br>
                         カップルからファミリー・３世代までゆったり過ごせるワンランク上の<br class="pc">和洋室タイプのお部屋でございます。<br><br>
@@ -388,6 +369,56 @@ get_header(); ?>
                         <p>・ツインベッド（ベッド1200×2000） ＋ 畳スペース</p>
                         <p>・バスなし　※大浴場をご利用ください</p>
                         <p>・100インチ超の大画面スクリーンをお楽しみいただけます</p>
+                        <p>・当ホテルはお部屋の布団敷きはセルフサービスとなります</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+        <!-- トドラーファミリールーム --> 
+        <div class="content bg_b_c room6">
+            <div class="inner img_bg_3">
+                <div class="con-sp-fv fadein">
+                    <div class="float-l con_mv">
+                        <div class="zoom" style="border-radius: 3.8rem;">
+                            <img src="<?php bloginfo('template_directory'); ?>/image/room_photo_main_toddlerfamily.jpg" alt="">
+                        </div>
+                        <div class="pc room_point">
+                            <p>・キングサイズのローベッド（180センチ×210センチ）</p>
+                            <p>・ユニットバス</p>
+                            <p>・流し台</p>
+                            <p>・電子レンジ、哺乳瓶消毒容器など未就学児連れファミリーに嬉しい備品</p>
+                            <p>・空気清浄機</p>
+                            <p>・当ホテルはお部屋の布団敷きはセルフサービスとなります</p>
+                        </div>
+                    </div>
+                    <div class="float-r con_text-r">
+                        <div class="con_title">
+                            <p>未就学児（トドラー）連れのファミリーにぴったり</p>
+                            <h2>トドラーファミリールーム</h2>
+                        </div>
+                    </div>
+                </div>
+                <div class="float-r con_text-r fadein">
+                    <p class="text">
+                    2024年5月にオープンする未就学児（トドラー）お子様連れファミリーにぴったりの新しいお部屋です。<br>
+                    10畳の和室に広々キングサイズ（幅180センチ）のローベッドをご用意。ユニットバス付、その他幼児向けの備品をご用意しております。もちろん大人2名様でのご利用でも快適にご利用いただけます。<br><br>
+                        定員：2名～3名(お子様は幼児のみ受入)　／　広さ：10畳&#178;
+                    </p>
+                    <a href="#" class="flex">
+                        <p class="btn"><span class="arrow"></span>このお部屋のプランはこちら</p>
+                    </a>
+                    <!-- <ul class="slick">
+                        <li><img src="<?php bloginfo('template_directory'); ?>/image/room_photo_3_1.png"></li>
+                        <li><img src="<?php bloginfo('template_directory'); ?>/image/room_photo_3_2.png"></li>
+                        <li><img src="<?php bloginfo('template_directory'); ?>/image/room_photo_3_3.png"></li>
+                    </ul> -->
+                    <div class="sp room_point">
+                        <p>・キングサイズのローベッド（180センチ×210センチ）</p>
+                        <p>・ユニットバス</p>
+                        <p>・流し台</p>
+                        <p>・電子レンジ、哺乳瓶消毒容器など未就学児連れファミリーに嬉しい備品</p>
+                        <p>・空気清浄機</p>
                         <p>・当ホテルはお部屋の布団敷きはセルフサービスとなります</p>
                     </div>
                 </div>
