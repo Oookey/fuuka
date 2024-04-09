88a7387a63e69187e20ec70db3ec2eb2f356aaa5
平賀 舞子 <hiraga@oookey.co.jp>
2024-04-09 17:00:08 +0900

    【WP】ポータルサイト修正

--- wp-content/themes/fuuka/page-portal.php
+++ wp-content/themes/fuuka/page-portal.php
@@ -20,28 +20,30 @@ get_header(); ?>
                             <p class="news_info">
                                 この度はホテルのご予約をいただき誠にありがとうございます。<br>
                                 事前にチェックイン手続きが可能な「スマートチェックイン」へのご協力をお願いしております。<br>
-                                <a href="<?php bloginfo('url'); ?>/smartcheckin_form">「スマートチェックイン」</a>をしていただきますと、到着時の署名や館内説明を省略させていただきますので、当日はスムーズにお部屋へご入室いただけ、チェックイン時のおもてなしを快適にご堪能いただけます。<br>
+                                「スマートチェックイン」をしていただきますと、到着時の署名や館内説明を省略させていただきますので、当日はスムーズにお部屋へご入室いただけ、チェックイン時のおもてなしを快適にご堪能いただけます。<br>
                                 また当日ご不明点がありましたらフロントにお尋ねください。
                             </p>
                             <p class="news_info indent2">
-                                <b>1.<a href="<?php bloginfo('url'); ?>/smartcheckin_form">「チェックイン入力フォーム」</a>に入力</b><br>
+                                <b>１）「<a href="<?php bloginfo('url'); ?>/smartcheckin_form">チェックイン入力フォーム</a>」に入力</b><br>
                                 「チェックイン入力フォーム」に必要事項をご入力いただき送信してください。<br>
                                 お客様に入力いただいた情報はメールにてお送りさせていただきます。<br>
                                 こちらは宿泊日前日までに情報を入力し送信してください。<br>
                             </p>
                             <p class="news_info indent2">
-                                <b>2.<a href="<?php bloginfo('url'); ?>/smartcheckin_form">「チェックインシート」</a>を当日持参</b><br>
+                                <b>２）「<a href="<?php bloginfo('url'); ?>/smartcheckin_form">チェックインシート</a>」を当日持参</b><br>
                                 添付ファイルを印刷しご記入いただき、当日スタッフにお渡しください。<br>
                             </p>
-                            <a href="https://mms.oookey.io/fuuka/news/" class="flex btn">
-                                <p class=""><span class="arrow"></span>全てのお知らせはこちら</p>
+                            <a href="" class="flex btn" style="float: none;display: block;margin-bottom: 4rem;">
+                                <p class=""><span class="arrow"></span>チェックイン入力フォームはこちら</p>
                             </a>
                             <p class="news_info indent2">
                                 ※下記の案内をご確認ください。すぐにチェックイン時のおもてなしを楽しみ、お部屋にお入りいただけるよう当日の説明は省かせていただきます。<br>
                             </p>
                             <p class="news_info">
-                                <b>【ホテル風かは大人も子供も楽しめるオールインクルーシブの宿です】<br>
-                                レストラン炙（あぶり）<br></b>
+                                <b>【ホテル風かは大人も子供も楽しめるオールインクルーシブの宿です】</b><br>
+                            </p>
+                            <p class="news_info">
+                                <b>レストラン炙（あぶり）</b><br>
                                 夕食時間：「一部17:30～19:00」「二部19:30～21:00」の2部制（事前予約要）<br>
                                 朝食時間：7:30～9:00（30分刻みで事前予約 又は 宿泊日当日にお伺い）
                             </p>
